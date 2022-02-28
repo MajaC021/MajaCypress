d31288d72040ced3576f71f3779bede8cdc492d7
MajaC021 <majacveticanin90@gmail.com>
2022-02-28 21:53:58 +0100

    Cypress commit

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+describe("login test", () => {
+    it("visit gallery page", () => {
+      cy.visit("/");
+    });
+  
+    it('click on the login button', () => {
+      cy.visit("/");
+      // jos jedan nacin za get-ovanje elementa
+      // cy.get('a[href="/login"]').click();
+      cy.get('.nav-link').eq(1).click();
+      cy.get('#email').type('majacveticanin90@gmail.com.com');
+      cy.get('#password').type('Majovita1990');
+      cy.get('button').click();
+
+    });
+    it('logout', () => {
+
+      // logout
+      cy.get('.nav-link').should('have.length', 3);
+      cy.get('.nav-link').eq(3).click();
+    })
+  
+  });
